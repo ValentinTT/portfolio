eb6a18460797453724759c64b9931f9f846f6137
ValentinTT <valentintapiatorti@gmail.com>
2024-06-30 18:12:05 -0300

    style(tailwind): :lipstick: update general styles and add prose styles for mdx

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import plugin from "tailwindcss/plugin";
+
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 
@@ -94,6 +96,12 @@ export default {
           "50%": { backgroundPosition: "100% 50%" },
         },
       },
+      lineHeight: {
+        prose: "1.8125",
+      },
+      maxWidth: {
+        prose: "768px",
+      },
       zIndex: {
         blur: "-1",
         grid: "-2",
@@ -104,5 +112,103 @@ export default {
   plugins: [
     require("@tailwindcss/typography"),
     require("tailwindcss-animated"),
+
+    // adds a `s-*` utility to apply the same width and height
+    // s stands for square
+    plugin(function sizePlugin(api) {
+      api.matchUtilities(
+        { s: (value) => ({ width: value, height: value }) },
+        { values: api.theme("width") }
+      );
+    }),
+    plugin(function ({ addComponents, theme }) {
+      addComponents({
+        ".container": {
+          "@apply w-full mx-auto max-w-screen-xl px-4 md:px-8": {},
+        },
+
+        ".grid-container": {
+          display: "grid",
+          gridTemplateColumns:
+            "1fr min(var(--container-width, 1280px), calc(100% - (2 * var(--container-gutter-size, 24px)))) 1fr",
+          gridColumnGap: "var(--container-gutter-size, 24px)",
+          overflow: "clip",
+          width: "100%",
+          rowGap: theme("spacing.24"),
+          paddingBottom: theme("spacing.24"),
+          "@media screen(md)": {
+            rowGap: theme("spacing.32"),
+            paddingBottom: theme("spacing.32"),
+          },
+          "@media screen(lg)": {
+            rowGap: theme("spacing.40"),
+            paddingBottom: theme("spacing.40"),
+          },
+
+          /* center all children by default */
+          "& > *": {
+            gridColumn: "2",
+          },
+
+          /* allows content to bleed edge to edge */
+          "& > .bleed-full": {
+            gridColumn: "1 / -1",
+          },
+
+          /* allows content to bleed to starting edge */
+          "& > .bleed-start": {
+            gridColumn: "1 / 3",
+          },
+
+          /* allows content to bleed to ending edge */
+          "& > .bleed-end": {
+            gridColumn: "2 / -1",
+          },
+
+          "& > .bleed-none": {
+            gridColumn: "2 !important",
+          },
+        },
+
+        ".accordion": {
+          "&::-webkit-details-marker": {
+            display: "none",
+          },
+        },
+
+        ".heading-1": {
+          "@apply text-5xl leading-tight": {},
+          fontWeight: "var(--heading-weight-normal)",
+        },
+
+        ".heading-2": {
+          "@apply text-4xl leading-tight": {},
+          fontWeight: "var(--heading-weight-normal)",
+        },
+
+        ".heading-3": {
+          "@apply text-2xl leading-tight": {},
+          fontWeight: "var(--heading-weight-normal)",
+        },
+
+        ".heading-4": {
+          "@apply text-xl leading-tight": {},
+          fontWeight: "var(--heading-weight-normal)",
+        },
+
+        ".heading-5": {
+          "@apply text-lg leading-tight": {},
+          fontWeight: "var(--heading-weight-normal)",
+        },
+
+        ".code": {
+          "@apply font-mono font-light": {},
+        },
+
+        ".panel": {
+          "@apply border border-accent-500 bg-accent-600 shadow-xl": {},
+        },
+      });
+    }),
   ],
 };
